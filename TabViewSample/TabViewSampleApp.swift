b2a02b0f990415472fdd2cf0a4a7d00a138e94a8
Ryuga <ribereo.minami@gmail.com>
2024-01-03 18:04:09 +0900

    Initial Commit

---
+++ TabViewSample/TabViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSampleApp.swift
+//  TabViewSample
+//
+//  Created by Ryuga on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
